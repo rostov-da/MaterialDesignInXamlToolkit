c42c40edf857a948161f5ab3bcfe70a61a7aed70
Rostov Dmitry <rostov.da@gmail.com>
2018-09-27 12:38:13 +0300

    Changed TextBox template. For TextBox added event handlers to the keyboard Eneter and Esc.

---
+++ MaterialDesignThemes.Wpf/Themes/MaterialDesignTheme.TextBox.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Input;
+
+namespace MaterialDesignThemes.Wpf
+{
+    public partial class MaterialDesignThemeTextBox
+    {
+        private void TextBox_KeyUp(object sender, KeyEventArgs e)
+        {
+            switch (e.Key)
+            {
+                case Key.Enter:
+                    {
+                        BindingExpression binding = GetBindingExpression();
+                        if (binding != null)
+                        {
+                            binding.UpdateSource();
+                        }
+                        break;
+                    }
+                case Key.Escape:
+                    {
+                        BindingExpression binding = GetBindingExpression();
+                        if (binding != null)
+                        {
+                            binding.UpdateTarget();
+                        }
+                        break;
+                    }
+            }
+
+            BindingExpression GetBindingExpression()
+            {
+                TextBox textBox = (TextBox)sender;
+                DependencyProperty property = TextBox.TextProperty;
+                BindingExpression binding = BindingOperations.GetBindingExpression(textBox, property);
+                return binding;
+            };
+        }
+    }
+}
